c00576de43da921eeaf561e25f6ecd53cbdb37df
Ecatalan\eduardo <eduardocatalan89@gmail.com>
2019-06-04 21:00:20 +0800

    update js composer

--- wp-content/plugins/js_composer/include/classes/editors/class-vc-backend-editor.php
+++ wp-content/plugins/js_composer/include/classes/editors/class-vc-backend-editor.php
@@ -18,7 +18,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  *
  * @since 4.2
  */
-class Vc_Backend_Editor implements Vc_Editor_Interface {
+class Vc_Backend_Editor {
 
 	/**
 	 * @var
@@ -51,6 +51,14 @@ class Vc_Backend_Editor implements Vc_Editor_Interface {
 		), 5 );
 		add_action( 'admin_print_scripts-post.php', array(
 			$this,
+			'registerScripts',
+		) );
+		add_action( 'admin_print_scripts-post-new.php', array(
+			$this,
+			'registerScripts',
+		) );
+		add_action( 'admin_print_scripts-post.php', array(
+			$this,
 			'printScriptsMessages',
 		) );
 		add_action( 'admin_print_scripts-post-new.php', array(
@@ -60,25 +68,28 @@ class Vc_Backend_Editor implements Vc_Editor_Interface {
 
 	}
 
+	public function registerScripts() {
+		$this->registerBackendJavascript();
+		$this->registerBackendCss();
+		// B.C:
+		visual_composer()->registerAdminCss();
+		visual_composer()->registerAdminJavascript();
+	}
+
 	/**
 	 *    Calls add_meta_box to create Editor block. Block is rendered by WPBakeryVisualComposerLayout.
 	 *
-	 * @see WPBakeryVisualComposerLayout
+	 * @param $post_type
+	 * @throws \Exception
 	 * @since  4.2
 	 * @access public
 	 *
-	 * @param $post_type
+	 * @see WPBakeryVisualComposerLayout
 	 */
 	public function render( $post_type ) {
 		if ( $this->isValidPostType( $post_type ) ) {
-			$this->registerBackendJavascript();
-			$this->registerBackendCss();
-			// B.C:
-			visual_composer()->registerAdminCss();
-			visual_composer()->registerAdminJavascript();
-
 			// meta box to render
-			add_meta_box( 'wpb_visual_composer', __( 'WPBakery Page Builder', 'js_composer' ), array(
+			add_meta_box( 'wpb_visual_composer', esc_html__( 'WPBakery Page Builder', 'js_composer' ), array(
 				$this,
 				'renderEditor',
 			), $post_type, 'normal', 'high' );
@@ -100,7 +111,7 @@ class Vc_Backend_Editor implements Vc_Editor_Interface {
 			return false;
 		}
 		$this->post = $post;
-		$post_custom_css = strip_tags( get_post_meta( $post->ID, '_wpb_post_custom_css', true ) );
+		$post_custom_css = wp_strip_all_tags( get_post_meta( $post->ID, '_wpb_post_custom_css', true ) );
 		$this->post_custom_css = $post_custom_css;
 		vc_include_template( 'editors/backend_editor.tpl.php', array(
 			'editor' => $this,
@@ -134,13 +145,15 @@ class Vc_Backend_Editor implements Vc_Editor_Interface {
 	 * @param string $type
 	 *
 	 * @return bool
+	 * @throws \Exception
 	 */
 	public function isValidPostType( $type = '' ) {
+		$type = ! empty( $type ) ? $type : get_post_type();
 		if ( 'vc_grid_item' === $type ) {
 			return false;
 		}
 
-		return vc_check_post_type( ! empty( $type ) ? $type : get_post_type() );
+		return apply_filters( 'vc_is_valid_post_type_be', vc_check_post_type( $type ), $type );
 	}
 
 	/**
@@ -171,7 +184,7 @@ class Vc_Backend_Editor implements Vc_Editor_Interface {
 			WPBakeryShortCodeFishBones::enqueueJs();
 		} else {
 			wp_enqueue_script( 'vc-backend-actions-js' );
-			$this->enqueueCss(); //needed for navbar @todo split
+			$this->enqueueCss(); // needed for navbar @todo split
 		}
 		do_action( 'vc_backend_editor_enqueue_js_css' );
 	}
@@ -183,10 +196,7 @@ class Vc_Backend_Editor implements Vc_Editor_Interface {
 			'backbone',
 			'underscore',
 		), WPB_VC_VERSION, true );
-		if (defined('JS_COMPOSER_THEME_ACTIVATED_URL'))
-			wp_register_script( 'vc-backend-min-js', vc_asset_url( 'js/dist/compat/backend.min.js' ), array( 'vc-backend-actions-js' ), WPB_VC_VERSION, true );
-		else
-			wp_register_script( 'vc-backend-min-js', vc_asset_url( 'js/dist/backend.min.js' ), array( 'vc-backend-actions-js' ), WPB_VC_VERSION, true );
+		wp_register_script( 'vc-backend-min-js', vc_asset_url( 'js/dist/backend.min.js' ), array( 'vc-backend-actions-js' ), WPB_VC_VERSION, true );
 		// used in tta shortcodes, and panels.
 		wp_register_script( 'vc_accordion_script', vc_asset_url( 'lib/vc_accordion/vc-accordion.min.js' ), array( 'jquery' ), WPB_VC_VERSION, true );
 		wp_register_script( 'wpb_php_js', vc_asset_url( 'lib/php.default/php.default.min.js' ), array( 'jquery' ), WPB_VC_VERSION, true );
@@ -194,7 +204,7 @@ class Vc_Backend_Editor implements Vc_Editor_Interface {
 		wp_register_script( 'wpb_json-js', vc_asset_url( 'lib/bower/json-js/json2.min.js' ), array(), WPB_VC_VERSION, true );
 		// used in post settings editor
 		wp_register_script( 'ace-editor', vc_asset_url( 'lib/bower/ace-builds/src-min-noconflict/ace.js' ), array( 'jquery' ), WPB_VC_VERSION, true );
-		wp_register_script( 'webfont', '//ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js' ); // Google Web Font CDN
+		wp_register_script( 'webfont', 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js', array(), WPB_VC_VERSION, true ); // Google Web Font CDN
 
 		wp_localize_script( 'vc-backend-actions-js', 'i18nLocale', visual_composer()->getEditorsLocale() );
 	}
@@ -218,7 +228,7 @@ class Vc_Backend_Editor implements Vc_Editor_Interface {
 			 * @todo check for usages
 			 * definetelly used in edit form param: css_animation, but curreny vc_add_shortcode_param doesn't accept css [ @todo refactor that ]
 			 */
-			wp_register_style( 'animate-css', vc_asset_url( 'lib/bower/animate-css/animate.min.css' ), array(), WPB_VC_VERSION );
+			wp_register_style( 'vc_animate-css', vc_asset_url( 'lib/bower/animate-css/animate.min.css' ), array(), WPB_VC_VERSION );
 		}
 	}
 
@@ -269,7 +279,7 @@ class Vc_Backend_Editor implements Vc_Editor_Interface {
 			// used in deprecated message and also in vc-icon shortcode
 			'font-awesome',
 			// used in css_animation edit form param
-			'animate-css',
+			'vc_animate-css',
 		);
 		$dependencies = array(
 			'js_composer',
@@ -286,6 +296,7 @@ class Vc_Backend_Editor implements Vc_Editor_Interface {
 
 	/**
 	 * @return bool
+	 * @throws \Exception
 	 */
 	public function editorEnabled() {
 		return vc_user_access()->part( 'backend_editor' )->can()->get();
